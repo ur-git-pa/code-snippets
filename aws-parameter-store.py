2cfed4762278ecf9bc1596a7642ef867f6c7ecc2
ur-git-pa <147447361+ur-git-pa@users.noreply.github.com>
2023-11-03 11:24:39 -0400

    Update aws-parameter-store.py

--- aws-parameter-store.py
+++ aws-parameter-store.py
@@ -1,3 +1,14 @@
+from dotenv import load_dotenv
+
+# Load environment variables from the "env.env" file in the current directory
+load_dotenv("env.env")
+
+# Access your environment variables
+import os
+aws_access_key_id = os.getenv('AWS_ACCESS_KEY_ID')
+aws_secret_access_key = os.getenv('AWS_SECRET_ACCESS_KEY')
+
+
 from __future__ import print_function
  
 import json
